5ed40c57da54669d80afcd73a89717418d64299f
Sergey <skilinlive@mail.ru>
2023-03-02 13:13:51 +0300

    поправил ссылку

--- controllers/StagePassportController.php
+++ controllers/StagePassportController.php
@@ -25,7 +25,7 @@ class StagePassportController extends Controller
         if ($this->request->get('step') == 'prev')
         {
             $this->users->update_user($this->user->id, array('stage_personal'=>0));
-            header('Location: /stage/personal');
+            header('Location: /stage/contact_persons');
             exit;
         }
         
