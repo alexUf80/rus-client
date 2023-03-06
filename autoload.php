459c0be515848ab271af624f8cca2d46e4bd9418
Андрей Попов <duircianos@yandex.ru>
2023-03-06 16:39:05 +0400

    infosphere inn

--- autoload.php
+++ autoload.php
@@ -12,5 +12,9 @@ function core_autoload($classname)
         require __DIR__.'/scorings/'.$classname.'.php';
     if (file_exists(dirname(__FILE__).'/vendor/autoload.php'))
         require dirname(__FILE__).'/vendor/autoload.php';
+    if (file_exists(__DIR__.'/tools/'.$classname.'.php'))
+        require __DIR__.'/tools/'.$classname.'.php';
+    if (file_exists(__DIR__.'/api/'.$classname.'.php'))
+        require __DIR__.'/api/'.$classname.'.php';
 }
 spl_autoload_register('core_autoload');
